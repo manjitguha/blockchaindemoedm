72a87a08952b239ab285447ec8954207c9e7644b
Manjit Guha <manjit.guha@gmail.com>
2016-12-28 18:06:26 +0530

    Completed

--- app.js
+++ app.js
@@ -2,12 +2,7 @@
  * Module dependencies.
  */
 
-var express = require('express'),
-    routes = require('./routes'),
-    user = require('./routes/user'),
-    http = require('http'),
-    path = require('path'),
-    fs = require('fs');
+var express = require('express'), routes = require('./routes'), user = require('./routes/user'), http = require('http'), path = require('path'), fs = require('fs');
 
 var app = express();
 
@@ -18,7 +13,7 @@ var cloudant;
 var fileToUpload;
 
 var dbCredentials = {
-    dbName: 'my_sample_db'
+	dbName : 'my_sample_db'
 };
 
 var bodyParser = require('body-parser');
@@ -35,7 +30,7 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended : true
 }));
 app.use(bodyParser.json());
 app.use(methodOverride());
@@ -44,44 +39,45 @@ app.use('/style', express.static(path.join(__dirname, '/views/css')));
 
 // development only
 if ('development' == app.get('env')) {
-    app.use(errorHandler());
+	app.use(errorHandler());
 }
 
 function initDBConnection() {
-    // When running on Bluemix, this variable will be set to a json object
-    // containing all the service credentials of all the bound services
-    if (process.env.VCAP_SERVICES) {
-        var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
-        // Pattern match to find the first instance of a Cloudant service in
-        // VCAP_SERVICES. If you know your service key, you can access the
-        // service credentials directly by using the vcapServices object.
-        for (var vcapService in vcapServices) {
-            if (vcapService.match(/cloudant/i)) {
-                dbCredentials.url = vcapServices[vcapService][0].credentials.url;
-            }
-        }
-    } else { // When running locally, the VCAP_SERVICES will not be set
+	// When running on Bluemix, this variable will be set to a json object
+	// containing all the service credentials of all the bound services
+	if (process.env.VCAP_SERVICES) {
+		var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+		// Pattern match to find the first instance of a Cloudant service in
+		// VCAP_SERVICES. If you know your service key, you can access the
+		// service credentials directly by using the vcapServices object.
+		for ( var vcapService in vcapServices) {
+			if (vcapService.match(/cloudant/i)) {
+				dbCredentials.url = vcapServices[vcapService][0].credentials.url;
+			}
+		}
+	} else { // When running locally, the VCAP_SERVICES will not be set
 
-        // When running this app locally you can get your Cloudant credentials
-        // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
-        // Variables section for an app in the Bluemix console dashboard).
-        // Alternately you could point to a local database here instead of a
-        // Bluemix service.
-        // url will be in this format:
+		// When running this app locally you can get your Cloudant credentials
+		// from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
+		// Variables section for an app in the Bluemix console dashboard).
+		// Alternately you could point to a local database here instead of a
+		// Bluemix service.
+		// url will be in this format:
 		// https://username:password@xxxxxxxxx-bluemix.cloudant.com
-        dbCredentials.url = "REPLACE ME";
-    }
+		dbCredentials.url = "REPLACE ME";
+	}
 
-    cloudant = require('cloudant')(dbCredentials.url);
+	cloudant = require('cloudant')(dbCredentials.url);
 
-    // check if DB exists if not create
-    cloudant.db.create(dbCredentials.dbName, function(err, res) {
-        if (err) {
-            console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
-        }
-    });
+	// check if DB exists if not create
+	cloudant.db.create(dbCredentials.dbName, function(err, res) {
+		if (err) {
+			console.log('Could not create new db: ' + dbCredentials.dbName
+					+ ', it might already exist.');
+		}
+	});
 
-    db = cloudant.use(dbCredentials.dbName);
+	db = cloudant.use(dbCredentials.dbName);
 }
 
 initDBConnection();
@@ -89,173 +85,208 @@ initDBConnection();
 app.get('/', routes.index);
 
 app.get('/api/users', function(request, response) {
-    console.log("/api/users method invoked.. ");
+	console.log("/api/users method invoked.. ");
 
-    db = cloudant.use(dbCredentials.dbName);
-    var userList = []; 
-    var i = 0;
-    db.list(function(err, body) {
-        if (!err) {
-            var len = body.rows.length;
-            console.log('total # of users -> ' + len);
-            body.rows.forEach(function(document) {
-            	  db.get(document.id, {
-                      revs_info: true
-                  }, function(err, user) {
-                      if (!err) {
-                    	  userList.push(user);
-                    	  i++;
-                    	  console.log('User is ->'+user);
-                    	  
-                    	  if (i >= len) {
-                    		  response.write(JSON.stringify({ status: 200,body: userList }));
-                    	    	
-                		      response.end(); 
-                		      console.log('ending response...');
-                          }
-                      }
-                      else{
-                    	  	response.write(JSON.stringify({ status: 200 }));
-                      		response.end(); 
-                      }
-                  });
-            	console.log('Adding User');
-            });
-        } else {
-            console.log(err);
-            response.write(JSON.stringify({ status: 200 }));
-        	response.end(); 
-        }
-    });
+	db = cloudant.use(dbCredentials.dbName);
+	var userList = [];
+	var i = 0;
+	db.list(function(err, body) {
+		if (!err) {
+			var len = body.rows.length;
+			console.log('total # of users -> ' + len);
+			body.rows.forEach(function(document) {
+				db.get(document.id, {
+					revs_info : true
+				}, function(err, user) {
+					if (!err) {
+						userList.push(user);
+						i++;
+						console.log('User is ->' + user);
+
+						if (i >= len) {
+							response.write(JSON.stringify({
+								status : 200,
+								body : userList
+							}));
+
+							response.end();
+							console.log('ending response...');
+						}
+					} else {
+						response.write(JSON.stringify({
+							status : 200
+						}));
+						response.end();
+					}
+				});
+				console.log('Adding User');
+			});
+		} else {
+			console.log(err);
+			response.write(JSON.stringify({
+				status : 200
+			}));
+			response.end();
+		}
+	});
 });
 
+app
+		.post(
+				'/api/authenticate',
+				function(request, response) {
+					console.log("/api/authenticate method invoked.. ");
 
+					var username = request.param('username');
+					var password = request.param('password');
+					console.log('username -->' + username);
+					console.log('password -->' + password);
 
+					if (!username && !password) {
+						username = request.body.username;
+						password = request.body.password;
+					}
+					console.log('username -->' + username);
+					console.log('password -->' + password);
 
-app.post('/api/authenticate', function(request, response) {
-    console.log("/api/authenticate method invoked.. ");
+					if (username && password) {
+						db = cloudant.use(dbCredentials.dbName);
+						var userList = [];
+						var i = 0;
+						db.find({
+							"selector" : {
+								"username" : {
+									"$eq" : username
+								},
+								"password" : {
+									"$eq" : password
+								}
+							},
+							"fields" : []
+						}, function(err, doc) {
+							if (!err) {
+								console.log('User --> ' + doc);
+								console.log('User Docs--> ' + doc.docs);
+								console.log('No of Users --> '
+										+ doc.docs.length);
+								if (doc.docs.length > 0) {
+									response.write(JSON.stringify({
+										status : 200,
+										body : {
+											token : 'fake-jwt-token',
+											loggedIdUser : doc.docs[0]
+										}
+									}));
+									response.end();
+								} else {
+									response.write(JSON.stringify({
+										status : 200
+									}));
+									response.end();
+								}
+							} else {
+								response.write(JSON.stringify({
+									status : 200
+								}));
+								response.end();
+							}
+						});
+					} else {
+						console
+								.log("/api/authenticate method invocation failed.. username or password is blank");
+						response.write(JSON.stringify({
+							status : 200
+						}));
+						response.end();
+					}
+				});
 
-    var username = request.param('username');
-    var password = request.param('password');
-    console.log('username -->'+username);
-    console.log('password -->'+password);
-    
-    if(!username && !password){
-    	username = request.body.username;
-        password = request.body.password;
-    }
-    console.log('username -->'+username);
-    console.log('password -->'+password);
-    
-    
-    if(username && password){
-	    db = cloudant.use(dbCredentials.dbName);
-	    var userList = [];
-	    var i = 0;
-	    db.find({
-	    	  "selector": {
-	    		    "username": {
-	    		      "$eq": username
-	    		    },
-	    		    "password": {
-	    		      "$eq": password
-	    		    }
-	    		  },
-	    		  "fields": [
-	    		  ]
-	    		}, 
-	    		function(err, doc) {
-	    			if(!err){
-	    				console.log('User --> '+doc);
-	    				console.log('User Docs--> '+doc.docs);
-	    				console.log('No of Users --> '+doc.docs.length);
-	    				if(doc.docs.length>0){
-							response.write(JSON.stringify({ status: 200, body: { token: 'fake-jwt-token' , loggedIdUser:doc.docs[0]} }));
-							response.end(); 
-	    				}
-	    				else{
-	    					response.write(JSON.stringify({ status: 200 }));
-		        		    response.end();
-	    				}
-	    			}
-	    			else{
-	    				response.write(JSON.stringify({ status: 200 }));
-	        		    response.end();
-	    			}
-	    		}
-	    	);
-	}
-    else{
-    	console.log("/api/authenticate method invocation failed.. username or password is blank");
-    	response.write(JSON.stringify({ status: 200 }));
-    	response.end(); 
-    }
-});
+app
+		.post(
+				'/api/patient',
+				function(request, response) {
+					console.log("/api/patient method invoked.. ");
 
+					var firstname = request.param('firstname');
+					var middlename = request.param('middlename');
+					var lastname = request.param('lastname');
+					var address = request.param('address');
+					var city = request.param('city');
+					var state = request.param('state');
+					var zip = request.param('zip');
+					var gender = request.param('gender');
+					var dateofbirth = request.param('dateofbirth');
 
-app.post('/api/patient', function(request, response) {
-    console.log("/api/patient method invoked.. ");
+					console.log('firstname -->' + firstname);
+					console.log('middlename -->' + middlename);
+					console.log('lastname -->' + lastname);
+					console.log('address -->' + address);
+					console.log('city -->' + city);
+					console.log('state -->' + state);
+					console.log('zip -->' + zip);
+					console.log('gender -->' + gender);
+					console.log('dateofbirth -->' + dateofbirth);
 
-    var username = request.param('username');
-    var password = request.param('password');
-    console.log('username -->'+username);
-    console.log('password -->'+password);
-    
-    if(!username && !password){
-    	username = request.body.username;
-        password = request.body.password;
-    }
-    console.log('username -->'+username);
-    console.log('password -->'+password);
-    
-    
-    if(username && password){
-	    db = cloudant.use(dbCredentials.dbName);
-	    var userList = [];
-	    var i = 0;
-	    db.find({
-	    	  "selector": {
-	    		    "username": {
-	    		      "$eq": username
-	    		    },
-	    		    "password": {
-	    		      "$eq": password
-	    		    }
-	    		  },
-	    		  "fields": [
-	    		  ]
-	    		}, 
-	    		function(err, doc) {
-	    			if(!err){
-	    				console.log('User --> '+doc);
-	    				console.log('User Docs--> '+doc.docs);
-	    				console.log('No of Users --> '+doc.docs.length);
-	    				if(doc.docs.length>0){
-							response.write(JSON.stringify({ status: 200, body: { token: 'fake-jwt-token' , loggedIdUser:doc.docs[0]} }));
-							response.end(); 
-	    				}
-	    				else{
-	    					response.write(JSON.stringify({ status: 200 }));
-		        		    response.end();
-	    				}
-	    			}
-	    			else{
-	    				response.write(JSON.stringify({ status: 200 }));
-	        		    response.end();
-	    			}
-	    		}
-	    	);
-	}
-    else{
-    	console.log("/api/patient method invocation failed.. username or password is blank");
-    	response.write(JSON.stringify({ status: 200 }));
-    	response.end(); 
-    }
-});
+					if (!firstname && !middlename && !lastname && !address
+							&& !city && !state && !zip && !gender
+							&& !dateofbirth) {
+						firstname = request.body.firstname;
+						middlename = request.body.middlename;
+						lastname = request.body.lastname;
+						address = request.body.address;
+						city = request.body.city;
+						state = request.body.state;
+						zip = request.body.zip;
+						gender = request.body.gender;
+						dateofbirth = request.body.dateofbirth;
+					}
+					console.log('firstname -->' + firstname);
+					console.log('middlename -->' + middlename);
+					console.log('lastname -->' + lastname);
+					console.log('address -->' + address);
+					console.log('city -->' + city);
+					console.log('state -->' + state);
+					console.log('zip -->' + zip);
+					console.log('gender -->' + gender);
+					console.log('dateofbirth -->' + dateofbirth);
 
+					if (firstname && middlename && lastname && address && city
+							&& state && zip && gender && dateofbirth) {
+						db = cloudant.use(dbCredentials.dbName);
 
+						db.insert({
+							firstname : firstname,
+							middlename : middlename,
+							lastname : lastname,
+							address : address,
+							city : city,
+							state : state,
+							zip : zip,
+							gender : gender,
+							dateofbirth : dateofbirth
+						}, '', function(err, doc) {
+							if (err) {
+								console.log(err);
+								response.sendStatus(500);
+							} else {
+								response.write(JSON.stringify({
+									status : 200,
+									patient : JSON.stringify(doc)
+								}));
+								response.end();
+							}
+						});
 
+					} else {
+						console
+								.log("/api/patient method invocation failed.. username or password is blank");
+						response.write(JSON.stringify({
+							status : 200
+						}));
+						response.end();
+					}
+				});
 
 http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
-    console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
